c18f29f5f9748b1015216ec62fe1ace4d88f5f0f
dairi256 <72542198+dairi256@users.noreply.github.com>
2025-12-29 17:43:32 +0800

    Add project files.

---
+++ S.E.N.T.I.N.E.L/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace S.E.N.T.I.N.E.L
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ S.E.N.T.I.N.E.L/Terminal.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace S.E.N.T.I.N.E.L
+{
+    internal class Terminal
+    {
+    }
+}
